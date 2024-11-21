df89a6cc9bd3ffd9000776ee489ce98dcd0c4cea
Phitan Kanpan <Tormiloveidom@gmail.com>
2024-11-21 13:35:16 +0700

    Add files via upload

---
+++ HOMEWOKE/HW1/HW1-3.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    char FirstName[20] = "";
+    char LastName[20] = "";
+
+    printf( "First Name: " );
+    scanf( "%s" ,&FirstName );
+    printf( "Last Name: " );
+    scanf( "%s" ,&LastName );
+    printf( "%s %s TC, RMUTL, Chiang Mai, Thailand" ,FirstName ,LastName );
+    return 0;
+}
